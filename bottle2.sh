1aa809d4b2e0261ac9fc637f0f2ec25e2214bab9
James <n5xrq@doublecluster.com>
2016-10-06 13:10:16 -0700

    added variables

--- bottle2.sh
+++ bottle2.sh
@@ -2,6 +2,11 @@
 
 # Does not need to be ran by sudo.
 
+# declare variables
+
+PYTHON_PATH=python3
+BOTTLE_PATH=$HOME/py/bottle2.py
+
 echo "Starting bottle webserver in background"
-nohup  python3 $HOME/py/bottle2.py &
+nohup  $PYTHON_PATH $BOTTLE_PATH &
 echo "Done"
