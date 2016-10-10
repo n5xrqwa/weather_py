abd3b25bc9cdd3d8baff0df5a7afb80d678a153c
James <n5xrq@doublecluster.com>
2016-10-10 08:39:08 -0700

    adding vars.sh

---
+++ vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Variable declarations
+#
+echo "Variable declaration"
+
+WEATHER_DIR="$HOME/weather_py"
+WEATHER_PATH="$WEATHER_DIR/static"
+WEATHER_DATA="$WEATHER_PATH/weather.html"
+PYTHON_PATH=python3
+BOTTLE_PATH="$WEATHER_DIR/bottle2.py"
+
+
+
